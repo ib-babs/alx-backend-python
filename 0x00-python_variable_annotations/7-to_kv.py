abf1537a11592a84bfc301656a85b7afcb029fe3
ib-babs <babatundeibrahim426@gmail.com>
2024-05-02 18:25:33 +0100

    Correcting task 7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 '''Module'''
-from typing import Union
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, Union[float, int]]:
     '''function to_kv that takes a string k
     and an int OR float v as arguments and
     returns a tuple.'''
--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -6,4 +6,3 @@ from typing import Iterable, List, Sequence, Tuple
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     '''Iterable elements returning Tuples of List and int'''
     return [(i, len(i)) for i in lst]
-
