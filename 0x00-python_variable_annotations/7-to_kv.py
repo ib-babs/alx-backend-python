5dd59ce312cffbce15fa6830e38ab3632fc1a4ce
ib-babs <babatundeibrahim426@gmail.com>
2024-05-02 18:33:07 +0100

    Correcting task 7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -3,7 +3,7 @@
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[str, Union[int, float]]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     '''function to_kv that takes a string k
     and an int OR float v as arguments and
     returns a tuple.'''
