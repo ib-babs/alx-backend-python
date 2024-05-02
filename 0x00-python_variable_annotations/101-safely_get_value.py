df1a51ad5d400645fce9bbc2e13fb8602b4bc3d3
ib-babs <babatundeibrahim426@gmail.com>
2024-05-02 19:35:33 +0100

    Adding 101* and 102* files - Fixed

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -4,7 +4,8 @@ from typing import Any, Union, TypeVar, Mapping
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[T, None] = None) -> Union[Any, T]:
     '''Annotation'''
     if key in dct:
         return dct[key]
--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
 '''Module'''
-from typing import Any, Tuple, Union, List
+from typing import Tuple, List
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
     '''Annotating using mypy'''
-    zoomed_in: Tuple = [
+    zoomed_in: List = [
         item for item in lst
         for i in range(int(factor))
     ]
     return zoomed_in
 
 
-array: list[int] = [12, 72, 91]
+array = (12, 72, 91)
 
-zoom_2x: List = zoom_array(array)
+zoom_2x = zoom_array(array)
 
-zoom_3x: Tuple = zoom_array(array, 3.0)
+zoom_3x: Tuple = zoom_array(array, 3)
