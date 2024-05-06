73c03a3864c86f6e851e37bb1ce9fa6c0076de81
ib-babs <babatundeibrahim426@gmail.com>
2024-05-06 19:22:07 +0100

    Annotating task4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 """Module"""
-import asyncio
+from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> list:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     '''New versiom 0f wait_n code'''
-    tasks = [task_wait_random(max_delay) for _ in range(n)]
-    return [await task for task in asyncio.as_completed(tasks)]
+    return [await task_wait_random(max_delay) for _ in range(n)]
