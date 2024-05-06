93dbec9031cfcb6af7c9560f715b344ef9327a91
ib-babs <babatundeibrahim426@gmail.com>
2024-05-06 18:59:49 +0100

    Initial commit

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -6,4 +6,5 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> list:
     '''New versiom 0f wait_n code'''
-    return [await task_wait_random(max_delay) for _ in range(n)]
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    return [await task for task in asyncio.as_completed(tasks)]
