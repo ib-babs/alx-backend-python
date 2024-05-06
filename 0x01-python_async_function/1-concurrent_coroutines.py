ea53537caf60c2e8a0f029d45bf9cd06ccb9a858
ib-babs <babatundeibrahim426@gmail.com>
2024-05-06 19:18:47 +0100

    Annotating task1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 """Module"""
-import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     '''Running concurrent coroutines running n-times'''
-    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
-    return [await task for task in asyncio.as_completed(tasks)]
+    return [await wait_random(max_delay) for _ in range(n)]
