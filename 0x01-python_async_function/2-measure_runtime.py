0c2e273902bcb1537685c21a075e31c988f0cca7
ib-babs <babatundeibrahim426@gmail.com>
2024-05-06 18:57:52 +0100

    Initial commit

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 """Module"""
+from time import time
+from asyncio import run
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
-    '''Measuring execution time'''
-    coroutine = await wait_n(n, max_delay)
-    total_time = sum(coroutine)
-    return total_time/n
+def measure_time(n: int, max_delay: int) -> float:
+    """ Measure the runtime """
+    start = time()
+
+    run(wait_n(n, max_delay))
+
+    end = time()
+
+    return (end - start) / n
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -7,7 +7,3 @@ task_wait_random = __import__('3-tasks').task_wait_random
 async def task_wait_n(n: int, max_delay: int) -> list:
     '''New versiom 0f wait_n code'''
     return [await task_wait_random(max_delay) for _ in range(n)]
-
-n = 5
-max_delay = 6
-print(asyncio.run(task_wait_n(n, max_delay)))
